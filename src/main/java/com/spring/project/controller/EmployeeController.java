f0a514e6499839a9ccd6d36d69edf2cca31e82f5
maia <maianeto2014@gmail.com>
2025-04-08 18:14:07 -0300

    Implemented EmployeeController Class

--- src/main/java/com/spring/project/controller/EmployeeController.java
+++ src/main/java/com/spring/project/controller/EmployeeController.java
@@ -3,9 +3,15 @@ package com.spring.project.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.project.model.Employee;
@@ -28,7 +34,21 @@ public class EmployeeController {
         return employeeService.findEmployeeById(id);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/")
+    public void createEmployee(@RequestBody Employee employee) {
+        employeeService.createEmployee(employee);
+    }
 
-    
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{id}")
+    public void updatedEmployee(@RequestBody Employee employee, @PathVariable Long id) {
+        employeeService.updateEmployee(employee, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+    }
 }
-
