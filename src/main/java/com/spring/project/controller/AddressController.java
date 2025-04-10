ab6163974cbc09d4342ffc744f339bef96c2d0c9
Jose Maia <maianeto2014@gmail.com>
2025-04-10 11:57:32 -0300

    Implemented AddressController class

--- src/main/java/com/spring/project/controller/AddressController.java
+++ src/main/java/com/spring/project/controller/AddressController.java
@@ -1,5 +1,51 @@
 package com.spring.project.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.project.model.Address;
+import com.spring.project.service.AddressService;
+
+@RestController
+@RequestMapping("/api/adresses")
 public class AddressController {
+    @Autowired
+    private AddressService addressService;
+
+    @GetMapping(value = "/")
+    public List<Address> findAllAddresses() {
+        return addressService.findAllAddresses();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Address findAddressById(Long id) {
+        return addressService.findAddressById(id);
+    }
     
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/")
+    public void createAdress(Address address) {
+        addressService.createAddress(address);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{id}")
+    public void updateAddress(Address address, Long id) {
+        addressService.updateAddress(address, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{id}")
+    public void deleteAddress(Long id) {
+        addressService.deleteAddress(id);
+    }
 }
