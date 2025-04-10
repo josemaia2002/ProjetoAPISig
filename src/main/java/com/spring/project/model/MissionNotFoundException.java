d345aa05973cd35829f2c3870576738e2a9aab1e
Jose Maia <maianeto2014@gmail.com>
2025-04-10 11:36:31 -0300

    Implemented MissionService class

---
+++ src/main/java/com/spring/project/model/MissionNotFoundException.java
@@ -0,0 +1,11 @@
+package com.spring.project.model;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class MissionNotFoundException extends RuntimeException{
+    public MissionNotFoundException() {
+        super("Mission not found");
+    }
+}
--- src/main/java/com/spring/project/service/MissionService.java
+++ src/main/java/com/spring/project/service/MissionService.java
@@ -1,10 +1,53 @@
 package com.spring.project.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spring.project.model.Mission;
+import com.spring.project.model.MissionNotFoundException;
+import com.spring.project.repository.MissionRepository;
+
 public class MissionService {
     // Find all ok
     // Find by id ok
-    // create ok
-    // update ok
+    // create 
+    // update 
     // delete
 
+    @Autowired
+    private MissionRepository missionRepository;
+
+    public List<Mission> findAllMissions() {
+        return missionRepository.findAll();
+    }
+
+    public Mission findMissionById(Long id) {
+        Optional<Mission> mission = missionRepository.findById(id);
+        if(mission.isEmpty()) {
+            throw new MissionNotFoundException();
+        }
+
+        return mission.get();
+    }
+
+    public void createMission(Mission mission) {
+        missionRepository.save(mission);
+    }
+
+    public void updateMission(Mission mission, Long id) {
+        Mission updatedMission = findMissionById(id);
+
+        updatedMission.setName(mission.getName());
+        updatedMission.setDuration(mission.getDuration());
+
+        missionRepository.save(updatedMission);
+    }
+
+    public void deleteMission(Long id) {
+        Mission mission = findMissionById(id);
+
+        missionRepository.delete(mission);
+    }
 }
